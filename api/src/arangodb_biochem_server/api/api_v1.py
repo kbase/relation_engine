98c32a9e448e1cea2bb8c4309de5af5b2ea215de
Jay R Bolton <jayrbolton@gmail.com>
2018-10-10 17:48:30 -0700

    Get a big start on making a swagger API with all the endpoints

--- api/src/arangodb_biochem_server/api/api_v1.py
+++ api/src/arangodb_biochem_server/api/api_v1.py
@@ -1,10 +1,71 @@
-"""The primary router for the Biochem API v1."""
+"""The primary router for the Relation Engine API v1."""
 import flask
+from flasgger import swag_from
 
 api_v1 = flask.Blueprint('api_v1', __name__)
 
 
-@api_v1.route('/', methods=['GET'])
-def root():
-    """Root route for the API which lists all paths."""
+@swag_from('create_view.yaml')
+@api_v1.route('/views', methods=['POST'])
+def create_view():
+    """See ./create_view.yaml for documentation."""
     return flask.jsonify({'test': True})
+
+
+@api_v1.route('/views', methods=['GET'])
+def query_view():
+    """
+    Run a saved query (a "view") using custom arguments.
+    ---
+    tags: ["views"]
+    parameters: []
+    responses:
+        200:
+            description: TODO
+            schema: {type: object}
+    """
+    return flask.jsonify([])
+
+
+@swag_from('create_schema.yaml')
+@api_v1.route('/schemas', methods=['POST'])
+def create_schema():
+    """See ./create_schema.yaml for documentation."""
+    return flask.jsonify({})
+
+
+@swag_from('fetch_schemas.yaml')
+@api_v1.route('/schemas', methods=['GET'])
+def fetch_schemas():
+    """See ./fetch_schemas.yaml for documentation."""
+    return flask.jsonify({})
+
+
+@api_v1.route('/documents', methods=['PUT'])
+def save_documents():
+    """
+    Create, update, or replace one or more documents in the database.
+    ---
+    tags: ["documents"]
+    parameters: []
+    responses:
+        200:
+            description: TODO
+            schema: {type: object}
+    """
+    return flask.jsonify({})
+
+
+@api_v1.route('/documents', methods=['DELETE'])
+def delete_documents():
+    """
+    Remove one or more documents from the database.
+    ---
+    tags: ["documents"]
+    parameters: []
+    responses:
+        200:
+            description: TODO
+            schema: {type: object}
+    """
+    return flask.jsonify({})
--- api/src/arangodb_biochem_server/app.py
+++ api/src/arangodb_biochem_server/app.py
@@ -1,7 +1,10 @@
 """The main entrypoint for running the Flask server."""
+import time
 import flask
 import os
+import subprocess
 from uuid import uuid4
+from flasgger import Swagger
 
 from .api.api_v1 import api_v1
 
@@ -12,11 +15,48 @@ app.url_map.strict_slashes = False  # allow both `get /v1/` and `get /v1`
 
 app.register_blueprint(api_v1, url_prefix='/v1')
 
+swagger_template = {
+    'swagger': '2.0',
+    'info': {
+        'title': 'Relation Engine API',
+        'description': 'API for working with the KBase Relation Engine graph database.',
+        'contact': {
+            'responsibleOrganization': 'DOE KBase',
+            'responsibleDeveloper': 'Jay R Bolton <jrbolton@lbl.gov>',
+            'email': 'scanon@lbl.gov',
+            'url': 'https://kbase.us'
+        },
+        'version': '1'
+    },
+    'schemes': ['https']
+}
+
+swagger = Swagger(app, template=swagger_template)
+
 
 @app.route('/', methods=['GET'])
 def root():
-    """Root path for the entire service; lists all API endpoints."""
-    return flask.jsonify({'test': True})
+    """
+    Root endpoint that gives server status.
+    ---
+    tags: ["root"]
+    parameters: []
+    responses:
+        200:
+            description: "An object of API version links in the form {version_name: version_path}"
+            schema: {type: object}
+        schema:
+            type: object
+            properties:
+                versions: {type: array, items: {type: string}}
+                server_time: {type: integer}
+                current_commit_hash: {type: string}
+    """
+    return flask.jsonify({
+        'versions': ['/v1'],
+        'server_time': int(time.time() * 1000),
+        'current_commit_hash': subprocess.check_output(['cat', '.git/refs/heads/master']).strip()
+    })
 
 
 @app.errorhandler(404)
