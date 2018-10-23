35299e96ac2aa6d432348b4885d0ffd86584c4bf
Jay R Bolton <jayrbolton@gmail.com>
2018-10-23 12:06:13 -0700

    Add some setuptools packaging boilerplate

---
+++ spec/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='relation_engine_spec',
+    version='0.1',
+    author='Jay Bolton',
+    author_email='jrbolton@lbl.gov',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_data={'': ['*.aql', '*.json5']},
+    license='MIT',
+    description='Specifications for the KBase Relation Engine API.',
+    url='https://kbase.us',
+    python_requires='>=3'
+)
