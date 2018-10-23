630c10de5997733c2d925b9a69193fcf62ef6920
Jay R Bolton <jayrbolton@gmail.com>
2018-10-23 10:31:30 -0700

    Get a prototype swagger api going

--- api/src/relation_engine_api/api/api_v1.py
+++ api/src/relation_engine_api/api/api_v1.py
@@ -1,46 +1,59 @@
 """The primary router for the Relation Engine API v1."""
+import time
+import subprocess
 import flask
 from flasgger import swag_from
 
 api_v1 = flask.Blueprint('api_v1', __name__)
 
 
-@swag_from('create_view.yaml')
-@api_v1.route('/views', methods=['POST'])
-def create_view():
-    """See ./create_view.yaml for documentation."""
+@api_v1.route('/', methods=['GET'])
+def root():
+    """
+    Server status.
+    ---
+    tags: ["root"]
+    parameters: []
+    responses:
+      200: {description: "Server status."}
+    """
+    return flask.jsonify({
+        'docs': '/apidocs',
+        'server_time': int(time.time() * 1000),
+        'current_commit_hash': subprocess.check_output(['cat', '.git/refs/heads/master']).strip()
+    })
+
+
+@swag_from('show_views.yaml')
+@api_v1.route('/views', methods=['GET'])
+def show_views():
+    """See ./show_views.yaml for documentation."""
     return flask.jsonify({'test': True})
 
 
-@swag_from('query_view.yaml')
-@api_v1.route('/views', methods=['GET'])
-def query_view():
-    """See ./query_view.yaml for documentation."""
+@swag_from('run_query.yaml')
+@api_v1.route('/query', methods=['GET'])
+def run_query():
+    """See ./run_query.yaml for documentation."""
     return flask.jsonify([])
 
 
-@swag_from('create_schema.yaml')
-@api_v1.route('/schemas', methods=['POST'])
-def create_schema():
-    """See ./create_schema.yaml for documentation."""
-    return flask.jsonify({})
-
-
-@swag_from('fetch_schemas.yaml')
+@swag_from('show_schemas.yaml')
 @api_v1.route('/schemas', methods=['GET'])
-def fetch_schemas():
-    """See ./fetch_schemas.yaml for documentation."""
+def show_schemas():
+    """See ./show_schemas.yaml for documentation."""
     return flask.jsonify({})
 
 
+@swag_from('save_documents.yaml')
 @api_v1.route('/documents', methods=['PUT'])
 def save_documents():
     """See ./save_documents.yaml for documentation."""
     return flask.jsonify({})
 
 
-@swag_from('delete_documents.yaml')
-@api_v1.route('/documents', methods=['DELETE'])
-def delete_documents():
-    """See ./delete_documents.yaml for documentation."""
-    return flask.jsonify({})
+# @swag_from('delete_documents.yaml')
+# @api_v1.route('/documents', methods=['DELETE'])
+# def delete_documents():
+#     """See ./delete_documents.yaml for documentation."""
+#     return flask.jsonify({})
--- api/src/relation_engine_api/app.py
+++ api/src/relation_engine_api/app.py
@@ -1,8 +1,6 @@
 """The main entrypoint for running the Flask server."""
-import time
 import flask
 import os
-import subprocess
 from uuid import uuid4
 from flasgger import Swagger
 
@@ -16,7 +14,7 @@ app.url_map.strict_slashes = False  # allow both `get /v1/` and `get /v1`
 app.register_blueprint(api_v1, url_prefix='/v1')
 
 swagger_template = {
-    'swagger': '3.0',
+    'openapi': '3.0.2',
     'info': {
         'title': 'Relation Engine API',
         'description': 'API for working with the KBase Relation Engine graph database.',
@@ -36,28 +34,15 @@ swagger = Swagger(app, template=swagger_template)
 
 @app.route('/', methods=['GET'])
 def root():
-    """
-    Root endpoint that gives server status.
-    ---
-    tags: ["root"]
-    parameters: []
-    responses:
-      200:
-        description: "An object of API version links in the form {version_name: version_path}"
-      schema:
-        type: object
-        properties:
-          versions: {type: array, items: {type: string}}
-          server_time: {type: integer}
-          current_commit_hash: {type: string}
-    """
-    return flask.jsonify({
-        'versions': ['/v1'],
-        'server_time': int(time.time() * 1000),
-        'current_commit_hash': subprocess.check_output(['cat', '.git/refs/heads/master']).strip()
-    })
+    """Redirects to the Swagger API docs."""
+    return flask.redirect('/apidocs')
 
 
 @app.errorhandler(404)
 def page_not_found(err):
     return (flask.jsonify({'status': 'error', 'error': '404 - Not found.'}), 404)
+
+
+@app.errorhandler(405)
+def method_not_allowed(err):
+    return (flask.jsonify({'status': 'error', 'error': '405 - Method not allowed.'}), 405)
