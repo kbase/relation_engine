2764a262c92d85a9c90505e95f6f06afe63e7169
Jay R Bolton <jayrbolton@gmail.com>
2018-10-25 15:42:41 -0700

    Generalize the small function that makes requests to arango

--- api/src/relation_engine_api/arango_utils/arango_requests.py
+++ api/src/relation_engine_api/arango_utils/arango_requests.py
@@ -11,17 +11,7 @@ db_user = os.environ.get('DB_USER', 'root')
 db_pass = os.environ.get('DB_PASS', 'password')
 
 
-test_query = """
-for doc in @@collection
-  collect with count into length
-  return length
-"""
-
-
 def make_arango_request(path, data):
-    data = {'query': test_query, 'bindVars': {'@collection': 'genes'}}
-    print('-' * 80)
-    print(db_url + path)
-    print(data)
+    """Make a generic arango request."""
     resp = requests.post(db_url + path, data=json.dumps(data), auth=(db_user, db_pass))
     return resp.text
