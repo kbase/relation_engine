d578f3c9e0b169f7749d79b88d90e0e1438f72a0
JamesJeffryes <jamesgjeffryes@gmail.com>
2019-02-05 11:46:09 -0600

    made a guincorn_conf so that specs can be downloaded. Moves most config there as well

--- api/scripts/start_server.sh
+++ api/scripts/start_server.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 set -e
 
-# Set the number of gevent workers to number of cores * 2 + 1
-# See: http://docs.gunicorn.org/en/stable/design.html#how-many-workers
-calc_workers="$(($(nproc) * 2 + 1))"
-# Use the WORKERS environment variable, if present
-workers=${WORKERS:-$calc_workers}
-
 gunicorn \
-  --worker-class gevent \
-  --timeout 1800 \
-  --workers $workers \
-  --bind :5000 \
-  ${DEVELOPMENT:+"--reload"} \
+  --config src/relation_engine_server/utils/gunicorn_conf.py \
   src.relation_engine_server.server:app
