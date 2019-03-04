b0f2b612f43e5cc662686930dbfafa7ec745ada8
Jay R Bolton <jayrbolton@gmail.com>
2019-03-04 10:15:35 -0800

    Add comment

--- api/src/relation_engine_server/utils/parse_json.py
+++ api/src/relation_engine_server/utils/parse_json.py
@@ -3,7 +3,10 @@ import flask
 
 
 def get_json_body():
-    """Parse json out of a request body, if present."""
+    """
+    Parse json out of a request body, if present.
+    If the request body is empty, we return None rather than throwing any parsing errors.
+    """
     json_body = None  # type: ignore
     req_data = flask.request.get_data()
     if req_data:
