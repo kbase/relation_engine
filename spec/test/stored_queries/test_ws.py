ffe60dbb8e505ae765ef2d1f318ce04d60ba3354
Jay R Bolton <jayrbolton@gmail.com>
2020-05-08 13:20:01 -0700

    Add some permissions checking in the big test case

--- spec/test/stored_queries/test_ws.py
+++ spec/test/stored_queries/test_ws.py
@@ -11,7 +11,7 @@ from test.helpers import get_config
 _CONF = get_config()
 
 
-def _ws_obj(wsid, objid, ver):
+def _ws_obj(wsid, objid, ver, is_public=True):
     """Create data for a dummy test workspace obj"""
     return {
         '_key': ':'.join((str(n) for n in (wsid, objid, ver))),
@@ -23,7 +23,7 @@ def _ws_obj(wsid, objid, ver):
         'size': 0,
         'epoch': 0,
         'deleted': False,
-        'is_public': True
+        'is_public': is_public,
     }
 
 
@@ -39,6 +39,9 @@ class TestWs(unittest.TestCase):
             _ws_obj(1, 2, 1),  # copy object
             _ws_obj(1, 3, 1),  # provenance object
             _ws_obj(1, 4, 1),  # reference object
+            _ws_obj(1, 5, 1, is_public=False),  # private copy obj
+            _ws_obj(1, 6, 1, is_public=False),  # private prov obj
+            _ws_obj(1, 7, 1, is_public=False),  # private ref obj
         ]
         create_test_docs('ws_object_version', ws_object_version)
         ws_type_version = [{'_key': 'Module.Type1-1.0'}]
@@ -50,20 +53,26 @@ class TestWs(unittest.TestCase):
         ]
         create_test_docs('ws_obj_instance_of_type', ws_obj_instance_of_type)
         ws_prov_descendant_of = [
-            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:3:1'}
+            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:3:1'},
+            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:6:1'},
         ]
         create_test_docs('ws_prov_descendant_of', ws_prov_descendant_of)
         ws_refers_to = [
-            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:4:1'}
+            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:4:1'},
+            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:7:1'},
         ]
         create_test_docs('ws_refers_to', ws_refers_to)
         ws_copied_from = [
-            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:2:1'}
+            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:2:1'},
+            {'_from': 'ws_object_version/1:1:1', '_to': 'ws_object_version/1:5:1'},
         ]
         create_test_docs('ws_copied_from', ws_copied_from)
 
     def test_fetch_related_data_valid(self):
-        """Test for the basic happy path."""
+        """
+        Test for the basic happy path.
+        This also covers the case of private-scope object results, which will be hidden from results.
+        """
         resp = requests.post(
             _CONF['re_api_url'] + '/api/v1/query_results',
             params={'stored_query': 'ws_fetch_related_data', 'show_public': True},
