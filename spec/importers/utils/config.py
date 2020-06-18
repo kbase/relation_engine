cc1fce5092c7f8bf33bcd676ce6146e4cf88ebca
Jay R Bolton <jayrbolton@gmail.com>
2020-06-18 16:21:28 -0700

    Simplify config further

--- spec/importers/utils/config.py
+++ spec/importers/utils/config.py
@@ -14,11 +14,10 @@ def load_from_env(extra_required=None, extra_optional=None, prefix='RES_'):
     conf = dict(DEFAULTS)
     required = list(REQUIRED) + (extra_required or [])
     optional = list(OPTIONAL) + (extra_optional or [])
-    for field in required + optional:
-        var = prefix + field
-        if var in os.environ:
-            conf[field] = os.environ[var]
-        elif field in required:
-            print(f"Missing required env var: {var}")
+    for field in required:
+        if (prefix + field) not in os.environ:
+            print(f"Missing required env var: {prefix + field}")
             exit(1)
+    for field in required + optional:
+        conf[field] = os.environ[prefix + field]
     return conf
