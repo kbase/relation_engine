dd97b86287487ac85de1dd650d58279e7304eb47
ialarmedalien <ialarmedalien@gmail.com>
2020-08-11 07:38:51 -0700

    Move pure validation tests to test_json_validation.py, and ensure tests cover schemas in yaml/json files and py data structures. Update README - small text fix.

--- relation_engine_server/test/test_json_validation.py
+++ relation_engine_server/test/test_json_validation.py
@@ -3,7 +3,6 @@ Test JSON validation functions
 """
 import unittest
 import os.path as os_path
-from yaml import safe_load
 from relation_engine_server.utils.json_validation import run_validator
 from jsonschema.exceptions import ValidationError
 from jsonpointer import JsonPointerException
@@ -17,7 +16,7 @@ test_schema = {
                 'name': {
                     'type': 'string',
                     'format': 'regex',
-                    'pattern': '^\w+$',
+                    'pattern': '^\\w+$',
                     'default': 'blank',
                 },
                 'distance': {
@@ -30,12 +29,6 @@ test_schema = {
                     'type': 'string',
                     'format': 'uri',
                 },
-                'creation_date': {
-                    'title': 'date of creation',
-                    'description': 'Approx six thousand years ago',
-                    'type': 'string',
-                    'format': 'date',
-                },
                 'date': {
                     'title': 'date',
                     'description': 'A type of dried fruit',
@@ -47,84 +40,14 @@ test_schema = {
     }
 }
 
-test_schema_two = {
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "title": "Exascale parser file manifest",
-  "type": "object",
-  "required": ["name", "file_list"],
-  "properties": {
-    "name": {
-      "title": "Dataset name",
-      "type": "string",
-      "description": "The name of the dataset",
-      "examples": ["Dan Jacobson Exascale dataset"]
-    },
-    "release_date": {
-      "title": "Release date",
-      "type": "string",
-      "description": "Date of the dataset release, in YYYY-MM-DD format",
-      "format": "date"
-    },
-    "notes": {
-      "type": "string",
-      "title": "Release notes",
-      "description": "Free text describing the release and any notes, or comments relevant to consumers of the data."
-    },
-    "file_list": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "required": ["data_type", "path"],
-        "oneOf": [
-          {
-            "properties": {
-              "data_type": {"enum": ["cluster"]}
-            },
-            "required": ["cluster_prefix"]
-          },
-          {
-            "properties": {
-              "data_type": {"enum": ["node", "edge"]}
-            }
-          }
-        ],
-        "properties": {
-          "data_type": {
-            "title": "Data type",
-            "type": "string",
-            "enum": ["node", "edge", "cluster"]
-          },
-          "date": {
-            "title": "File creation date",
-            "description": "date of file creation in the format YYYY-MM-DD",
-            "type": "string",
-            "format": "date"
-          },
-          "description": {
-            "title": "Description of the cluster set",
-            "type": "string"
-          },
-          "path": {
-            "title": "File path",
-            "type": "string"
-          },
-          "cluster_prefix": {
-            "title": "Prefix",
-            "type": "string",
-            "description": "The prefix to be used for clusters"
-          },
-          "title": {
-            "title": "Name of the cluster set",
-            "type": "string"
-          }
-        }
-      }
-    }
-  }
-}
-
-
 valid_json_loc = '/properties/params'
+test_data_dir = os_path.join('/app', 'relation_engine_server', 'test', 'data', 'json_validation')
+
+test_schema_list = [
+    ['schema', test_schema],
+    ['schema_file', os_path.join(test_data_dir, 'test_schema.json')],
+    ['schema_file', os_path.join(test_data_dir, 'test_schema.yaml')],
+]
 
 
 class TestJsonValidation(unittest.TestCase):
@@ -172,136 +95,202 @@ class TestJsonValidation(unittest.TestCase):
                 validate_at='/properties/params')
         self.assertEqual(output, {'name': 'name', 'distance': 3})
 
-    def test_json_validation_defaults(self):
+    def test_json_validation(self):
+
+        test_list = [
+            self.add_defaults,
+            self.pattern_validation,
+            self.uri_validation,
+            self.date_format_validation,
+        ]
+
+        for test_schema in test_schema_list:
+            schema_file_arg = schema_arg = test_schema[1]
+
+            if test_schema[0] == 'schema':
+                schema_file_arg = None
+            else:
+                schema_arg = None
+
+            for test in test_list:
+                test(schema_arg, schema_file_arg)
+
+    def add_defaults(self, schema_arg, schema_file_arg):
         """Test that the jsonschema validator sets default values."""
 
-        test_data = run_validator(schema=test_schema, data={}, validate_at=valid_json_loc)
+        test_data = run_validator(
+            schema=schema_arg,
+            schema_file=schema_file_arg,
+            data={},
+            validate_at=valid_json_loc)
         self.assertEqual(test_data, {'name': 'blank', 'distance': 1})
 
-    def test_pattern_validation(self):
+        for file_ext in ['json', 'yaml']:
+            file_path = os_path.join(test_data_dir, 'defaults.' + file_ext)
+            self.assertEqual(
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc
+                ),
+                {'name': 'blank', 'distance': 1}
+            )
+
+    def pattern_validation(self, schema_arg, schema_file_arg):
         '''Test pattern validation'''
 
         # validation error - string does not match regex
         err_str = "'Mr Blobby' does not match .*?"
         with self.assertRaisesRegex(ValidationError, err_str):
             run_validator(
-                schema=test_schema,
+                schema=schema_arg,
+                schema_file=schema_file_arg,
                 data={'name': 'Mr Blobby', 'distance': 3},
                 validate_at=valid_json_loc)
 
         # this string is OK
         output = run_validator(
-                schema=test_schema,
-                data={'name': 'Mr_Blobby_666', 'distance': 3},
-                validate_at=valid_json_loc)
+              schema=schema_arg,
+              schema_file=schema_file_arg,
+              data={'name': 'Mr_Blobby_666', 'distance': 3},
+              validate_at=valid_json_loc)
         self.assertEqual(output, {'name': 'Mr_Blobby_666', 'distance': 3})
 
-    def test_uri_validation(self):
+        for file_ext in ['json', 'yaml']:
+            # validation error - string does not match regex
+            err_str = '"what\'s-the-problem with-this-string\?" does not match .*?'
+            file_path = os_path.join(test_data_dir, 'invalid_pattern.' + file_ext)
+            with self.assertRaisesRegex(ValidationError, err_str):
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc)
+
+            file_path = os_path.join(test_data_dir, 'valid_pattern.' + file_ext)
+            self.assertEqual(
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc),
+                {'name': 'No_problem_with_this_string', 'distance': 3}
+            )
+
+    def uri_validation(self, schema_arg, schema_file_arg):
         '''Test URI validation is operational'''
 
         err_str = "'where is it\?' is not a 'uri'"
         with self.assertRaisesRegex(ValidationError, err_str):
             run_validator(
-                schema=test_schema,
+                schema=schema_arg,
+                schema_file=schema_file_arg,
                 data={'name': 'blank', 'distance': 3, 'home_page': 'where is it?'},
                 validate_at=valid_json_loc)
 
         # this string is OK
-        input = {'name': 'blank', 'distance': 3, 'home_page': 'http://www.home/com'}
+        input = {'name': 'valid_uri', 'distance': 3, 'home_page': 'http://www.home.com'}
         output = run_validator(
-                schema=test_schema,
-                data=input,
-                validate_at=valid_json_loc)
+            schema=schema_arg,
+            schema_file=schema_file_arg,
+            data=input,
+            validate_at=valid_json_loc)
         self.assertEqual(output, input)
 
-    def test_date_format_validation(self):
+        # data files
+        for file_ext in ['json', 'yaml']:
+            file_path = os_path.join(test_data_dir, 'invalid_uri.' + file_ext)
+            err_str = "'where is it\?' is not a 'uri'"
+            with self.assertRaisesRegex(ValidationError, err_str):
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc)
+
+            file_path = os_path.join(test_data_dir, 'valid_uri.' + file_ext)
+            self.assertEqual(
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc),
+                {
+                    "name": "valid_uri",
+                    "distance": 3,
+                    "home_page": "http://json-validation.com:5000/this/is/valid"
+                }
+            )
+
+    def date_format_validation(self, schema_arg, schema_file_arg):
         '''ensure that fancy date formats are correctly validated'''
 
-        err_str = "'12345678' is not a 'date'"
+        err_str = "'202001017' is not a 'date'"
         with self.assertRaisesRegex(ValidationError, err_str):
-            input = {'name': 'whatever', 'distance': 1, 'creation_date': '12345678'}
+            input = {'name': 'whatever', 'distance': 1, 'date': '202001017'}
             run_validator(
-                schema=test_schema,
+                schema=schema_arg,
+                schema_file=schema_file_arg,
                 data=input,
                 validate_at=valid_json_loc)
 
-        # date field NAMED date!
-        err_str = "'12345678' is not a 'date'"
-        with self.assertRaisesRegex(ValidationError, err_str):
-            input = {'name': 'whatever', 'distance': 1, 'date': '12345678'}
-            run_validator(
-                schema=test_schema,
-                data=input,
-                validate_at=valid_json_loc)
-
-        input = {'name': 'whatever', 'distance': 1, 'creation_date': '2020-05-23'}
+        input = {'name': 'whatever', 'distance': 1, 'date': '2020-05-23'}
         output = run_validator(
-            schema=test_schema,
+            schema=schema_arg,
+            schema_file=schema_file_arg,
             data=input,
             validate_at=valid_json_loc)
         self.assertEqual(input, output)
 
-        # use the manifest schema
-        with self.assertRaisesRegex(ValidationError, err_str):
-            run_validator(
-                schema=test_schema_two,
-                data={'name': 'a', 'file_list': [], 'release_date': '12345678'}
+        # data files
+        for file_ext in ['json', 'yaml']:
+            # invalid type (number instead of string)
+            file_path = os_path.join(test_data_dir, 'invalid_date_type.' + file_ext)
+            err_str = "20200606 is not of type 'string'"
+            with self.assertRaisesRegex(ValidationError, err_str):
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc)
+
+            # quoted string but not in the correct format
+            file_path = os_path.join(test_data_dir, 'invalid_date.' + file_ext)
+            err_str = "'20200606' is not a 'date'"
+            with self.assertRaisesRegex(ValidationError, err_str):
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc)
+
+            file_path = os_path.join(test_data_dir, 'valid_date.' + file_ext)
+            self.assertEqual(
+                run_validator(
+                    schema=schema_arg,
+                    schema_file=schema_file_arg,
+                    data_file=file_path,
+                    validate_at=valid_json_loc),
+                {
+                    "name": "valid_date",
+                    "date": "2020-06-06",
+                    "distance": 3,
+                }
             )
 
+        # pyyaml-specific issue: dates get automatically parsed into datetime objects (doh!)
+        file_path = os_path.join(test_data_dir, 'unquoted_date.yaml')
+        err_str = "datetime.date\(2020, 6, 6\) is not of type 'string'"
         with self.assertRaisesRegex(ValidationError, err_str):
             run_validator(
-                schema_file='/app/importers/djornl/manifest.schema.json',
-                data={'name': 'a', 'file_list': [], 'release_date': '12345678'}
-            )
-
-        # valid inputs
-        more_input = {'name': 'a', 'file_list': [], 'release_date': '1999-12-31'}
-        more_output = run_validator(
-            schema=test_schema_two,
-            data=more_input
-        )
-        self.assertEqual(more_input, more_output)
-
-        more_output = run_validator(
-            schema_file='/app/importers/djornl/manifest.schema.json',
-            data=more_input
-        )
-        self.assertEqual(more_input, more_output)
-
-    def test_valid_manifest_validation(self):
-        """ensure that a valid manifest passes validation"""
-
-        manifest_schema = '/app/importers/djornl/manifest.schema.json'
-        test_dir = '/app/spec/test/djornl/'
-        with open(os_path.join(test_dir, 'test_data', 'manifest.yaml')) as fd:
-            file_contents = safe_load(fd)
-
-        # raw data
-        manifest_data = run_validator(
-            schema_file=manifest_schema,
-            data=file_contents,
-            nicer_errors=True
-        )
-        self.assertTrue(manifest_data)
-
-        # data file
-        manifest_data = run_validator(
-            schema_file=manifest_schema,
-            data_file=os_path.join(test_dir, 'test_data', 'manifest.yaml'),
-            nicer_errors=True
-        )
-        self.assertTrue(manifest_data)
-
-        # data file
-        manifest_data = run_validator(
-            schema_file=manifest_schema,
-            data_file=os_path.join(test_dir, 'valid', 'with_descriptions.yaml'),
-            nicer_errors=True
-        )
-        self.assertTrue(manifest_data)
+                schema=schema_arg,
+                schema_file=schema_file_arg,
+                data_file=file_path,
+                validate_at=valid_json_loc)
 
     def test_schema_references(self):
-        """ensure referenced schemas can be accessed"""
+        """Ensure referenced schemas, including those written in yaml, can be accessed."""
 
         valid_edge_data = {
             "_from": "here",
@@ -319,7 +308,7 @@ class TestJsonValidation(unittest.TestCase):
             "edge_type": "whatever",
         }
 
-        schema_ref_dir = ['/app', 'spec', 'test', 'sample_schemas', 'schema_refs']
+        schema_ref_dir = ['/app', 'relation_engine_server', 'test', 'data', 'schema_refs']
 
         # same schema in different places
         path_list = [
