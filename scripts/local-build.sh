6b911a420dd6f420bba27fd4692c515dc513d2a6
ialarmedalien <ialarmedalien@gmail.com>
2020-08-26 09:50:19 -0700

    Removing hooks folder and consolidating local build script into scripts/local-build.sh

--- scripts/local-build.sh
+++ scripts/local-build.sh
@@ -2,6 +2,18 @@
 set -e
 # show the commands we execute
 set -o xtrace
-export IMAGE_NAME="kbase/relation_engine_api:local_build"
-sh hooks/build
+
+# $IMAGE_NAME var is injected into the build so the tag is correct.
+if [ -z "$IMAGE_NAME" ]; then
+  export IMAGE_NAME="kbase/relation_engine_api:latest"
+fi
+
+export BRANCH=`git symbolic-ref --short HEAD`
+export DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+export COMMIT=`git rev-parse --short HEAD`
+docker build --file Dockerfile
+             --build-arg BUILD_DATE=$DATE \
+             --build-arg VCS_REF=$COMMIT \
+             --build-arg BRANCH=$BRANCH \
+             -t ${IMAGE_NAME} .
 docker push $IMAGE_NAME
