699f336438bd68d390c75423a44d085fbabb9488
Jay R Bolton <jayrbolton@gmail.com>
2020-09-17 15:52:10 -0700

    Add newline break char

--- scripts/local-build.sh
+++ scripts/local-build.sh
@@ -11,7 +11,7 @@ fi
 export BRANCH=`git symbolic-ref --short HEAD`
 export DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 export COMMIT=`git rev-parse --short HEAD`
-docker build --file Dockerfile
+docker build --file Dockerfile \
              --build-arg BUILD_DATE=$DATE \
              --build-arg VCS_REF=$COMMIT \
              --build-arg BRANCH=$BRANCH \
