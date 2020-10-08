465566d9b8f12a023ee666db80bee10cab0bbdc0
Jay R Bolton <jayrbolton@gmail.com>
2020-10-08 15:14:42 -0700

    Fix docker-compose; gitignore test coverage dir; clean up test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,17 +9,13 @@ set -e
 flake8 --max-complexity 15 /app
 mypy --ignore-missing-imports /app
 bandit -r /app
-rm -rf /spec
-mkdir /spec
-mkdir /spec/repo
-cp -r /app/spec/* /spec/repo/
 # start server, using the specs in /spec/repo
 sh /app/scripts/start_server.sh &
 coverage erase
 # spec validation
-python -m spec.validate &&
+python -m spec.validate
 # run importer/, relation_engine_server/, and spec/ tests
-coverage run --branch -m unittest discover -v &&
+coverage run --branch -m unittest discover -v
 # RE client tests
-PYTHONPATH=client_src python -m unittest discover client_src/test &&
+PYTHONPATH=client_src python -m unittest discover client_src/test
 coverage html --omit=*/test_*
