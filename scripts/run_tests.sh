db8bed48ff02ddafc101ac750df901fe537f6347
Erik Pearson <eapearson@lbl.gov>
2021-05-07 09:07:30 -0700

    quote env variables

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,9 +9,9 @@ set -e
 # Run code quality checks before tests; a failure here
 # will prevent the tests from running.
 source_dirs="/app/relation_engine_server /app/client_src /app/spec"
-flake8 --max-complexity 20 $source_dirs
-mypy --ignore-missing-imports $source_dirs
-bandit -r $source_dirs
+flake8 --max-complexity 20 "$source_dirs"
+mypy --ignore-missing-imports "$source_dirs"
+bandit -r "$source_dirs"
 
 # start server, using the specs in /spec/repo
 sh /app/scripts/start_server.sh &
