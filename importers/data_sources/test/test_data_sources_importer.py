434943dc374f066db3e5394c398829b430790cfa
Erik Pearson <eapearson@lbl.gov>
2021-08-16 05:56:57 -0700

    fix unit tests

--- importers/data_sources/test/test_data_sources_importer.py
+++ importers/data_sources/test/test_data_sources_importer.py
@@ -8,12 +8,41 @@ import responses
 from importers.data_sources.importer import \
     Importer, note, do_import, main, get_args, \
     init, get_relative_dir
-from spec.test.helpers import modified_environ
 
-API_URL = 'http://localhost:5000'
+RE_API_URL = 'http://localhost:5000'
+AUTH_URL = 'http://auth:5000'
+AUTH_TOKEN = 'admin_token'
+
+ME_RESPONSE = {
+    'created': 1578347292120,
+    "lastlogin": 1628886788908,
+    "display": "KBase UI Test User",
+    "roles": [],
+    "customroles": [],
+    "policyids": [
+        {
+            "id": "data-policy.1",
+            "agreedon": 1578347292120
+        },
+        {
+            "id": "kbase-user.1",
+            "agreedon": 1578347292120
+        }
+    ],
+    "user": "kbaseuitest",
+    "local": False,
+    "email": "eapearson+kbaseuitest@lbl.gov",
+    "idents": [
+        {
+            "provusername": "kbaseuitest@globusid.org",
+            "provider": "Globus",
+            "id": "91436b502ec80a8165c6691ca8a64029"
+        }
+    ]
+}
 
 
-def make_importer(data_path, use_env_data=True):
+def make_importer(data_path, use_env_data=True, dry_run=None, auth_token=AUTH_TOKEN):
     # Data files are located in the test directory
     dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -21,13 +50,9 @@ def make_importer(data_path, use_env_data=True):
     # ... and in the specified data path
     test_data_path = os.path.join(dir_path, "data", data_path)
     print('\nTEST DATA PATH', test_data_path, use_env_data)
-    if use_env_data:
-        with modified_environ(RES_ROOT_DATA_PATH=test_data_path):
-            importer = Importer()
-            return importer
-    else:
-        importer = Importer()
-        return importer
+
+    return Importer(re_api_url=RE_API_URL, data_dir=test_data_path, dry_run=dry_run,
+                    auth_token=auth_token)
 
 
 class TestDataSourcesFunctions(unittest.TestCase):
@@ -65,55 +90,30 @@ class TestDataSourcesFunctions(unittest.TestCase):
 
 
 class MyArgs:
-    def __init__(self, dry_run, quiet):
+    def __init__(self, dry_run, quiet, re_api_url, auth_token, data_dir):
         self.dry_run = dry_run
         self.quiet = quiet
+        self.re_api_url = re_api_url
+        self.auth_token = auth_token
+        self.data_dir = data_dir
 
 
 class TestDataSourcesImporter(unittest.TestCase):
-    def test_get_config(self):
-        """
-        The `get_config` method does returns either the 
-        requested config value, or a default value
-        """
-        imp = make_importer('standard')
-        # The `API_URL` key should be defined.
-        self.assertIsNotNone(imp.get_config('API_URL', None))
-        # But "foo" not, and None is a good default value signal.
-        self.assertIsNone(imp.get_config('foo', None))
-        # But "foo" not, leave out default value.
-        self.assertIsNone(imp.get_config('foo', None))
-        # Provide a silly default values.
-        # But "foo" not
-        self.assertEquals(imp.get_config('foo', 'silly'), 'silly')
-
-    def test_get_config_or_fail(self):
-        """
-        The `get_config_or_fail` method, on the other hand, raises a
-        `KeyError` if the given config key does not exist.
-        """
-        imp = make_importer('standard')
-        # The `API_URL` key should be defined.
-        self.assertIsNotNone(imp.get_config_or_fail('API_URL'))
-        # But the `foo` key is not, and should raise
-        with self.assertRaises(KeyError):
-            imp.get_config_or_fail('foo')
-
     def test_load_data_dry_run(self):
         """
         A dry run load using standard test data.
         """
-        imp = make_importer('standard')
+        imp = make_importer('standard', dry_run=True)
         self.assertTrue(imp)
-        result = imp.load_data(dry_run=True)
+        result = imp.load_data()
         self.assertTrue(result)
 
     def test_load_data_dry_run_default_data_dir(self):
         """
         A dry run load which uses the canonical (built-in) data
         """
-        imp = make_importer('standard', use_env_data=False)
-        result = imp.load_data(dry_run=True)
+        imp = make_importer('standard', use_env_data=False, dry_run=True)
+        result = imp.load_data()
         self.assertTrue(result)
 
     def test_load_data_dry_run_bad_dir(self):
@@ -121,18 +121,19 @@ class TestDataSourcesImporter(unittest.TestCase):
         A dry run load which uses a bad data loading directory.
         (Really a test of our test helper function.)
         """
-        imp = make_importer('bad_dir')
+        imp = make_importer('bad_dir', dry_run=True)
         with self.assertRaises(Exception):
-            imp.load_data(dry_run=True)
+            imp.load_data()
 
     def test_load_data_bad_ns(self):
         """
         Dry and non-dry runs using load data with bad namespaces (ns)
         """
-        imp = make_importer('bad_ns')
-        result = imp.load_data(dry_run=True)
+        imp = make_importer('bad_ns', dry_run=True)
+        result = imp.load_data()
         self.assertFalse(result)
-        result = imp.load_data(dry_run=False)
+        imp2 = make_importer('bad_ns', dry_run=False)
+        result = imp2.load_data()
         self.assertFalse(result)
 
     @responses.activate
@@ -140,26 +141,29 @@ class TestDataSourcesImporter(unittest.TestCase):
         """
         Non-dry run load using a mocked RE API endpoint which should succeed.
         """
-        responses.add(responses.PUT, f'{API_URL}/api/v1/documents',
+        responses.add(responses.PUT, f'{RE_API_URL}/api/v1/documents',
                       json={'do_not': 'care'}, status=200)
-        imp = make_importer('standard')
-        result = imp.load_data(dry_run=False)
+        responses.add(responses.PUT, f'{AUTH_URL}/api/v2/me',
+                      json=ME_RESPONSE, status=200)
+        imp = make_importer('standard', dry_run=False)
+        result = imp.load_data()
         self.assertTrue(result)
 
     @responses.activate
     def test_load_data_non_dry_run_mocked_error_response(self):
         """
-        Non-dry run load using a moocked RE API endpoint which
+        Non-dry run load using a mocked RE API endpoint which
         should fail.
         """
-        responses.add(responses.PUT, f'{API_URL}/api/v1/documents',
+        responses.add(responses.PUT, f'{RE_API_URL}/api/v1/documents',
                       json={'do_not': 'care'}, status=400)
-        imp = make_importer('standard')
+        imp = make_importer('standard', dry_run=False, auth_token=AUTH_TOKEN)
         with self.assertRaises(RuntimeError):
-            imp.load_data(dry_run=False)
+            imp.load_data()
 
     def test_do_import_dry_run(self):
-        do_import(dry_run=True)
+        do_import(dry_run=True, data_dir=None, re_api_url=RE_API_URL,
+                  auth_token=AUTH_TOKEN)
         self.assertTrue(True)
 
     @responses.activate
@@ -167,9 +171,10 @@ class TestDataSourcesImporter(unittest.TestCase):
         """
         Non-dry-run load should be happy with a 200 response
         """
-        responses.add(responses.PUT, f'{API_URL}/api/v1/documents',
+        responses.add(responses.PUT, f'{RE_API_URL}/api/v1/documents',
                       json={'do_not': 'care'}, status=200)
-        do_import(dry_run=False)
+        do_import(dry_run=False, data_dir=None, re_api_url=RE_API_URL,
+                  auth_token=AUTH_TOKEN)
         self.assertTrue(True)
 
     @responses.activate
@@ -178,10 +183,11 @@ class TestDataSourcesImporter(unittest.TestCase):
         Non-dry-run load should exit with an error return code (1) if
         the attempt yields a 400 error response.
         """
-        responses.add(responses.PUT, f'{API_URL}/api/v1/documents',
+        responses.add(responses.PUT, f'{RE_API_URL}/api/v1/documents',
                       json={'do_not': 'care'}, status=400)
         with self.assertRaises(SystemExit) as se:
-            do_import(dry_run=False)
+            do_import(dry_run=False, data_dir=None, re_api_url=RE_API_URL,
+                      auth_token=AUTH_TOKEN)
         self.assertEqual(se.exception.code, 1)
 
     def test_main_dry_run(self):
@@ -189,7 +195,8 @@ class TestDataSourcesImporter(unittest.TestCase):
         Calling the main() entry point with with a dry run requested,
         using the default data path should succeed.
         """
-        my_get_args = MyArgs(dry_run=True, quiet=True)
+        my_get_args = MyArgs(dry_run=True, quiet=True, re_api_url=RE_API_URL,
+                             data_dir=None, auth_token=AUTH_TOKEN)
         with patch('importers.data_sources.importer.get_args',
                    return_value=my_get_args) as mock_get_args:
             with self.assertRaises(SystemExit) as se:
@@ -206,7 +213,9 @@ class TestDataSourcesImporter(unittest.TestCase):
                 cases.append({'dry_run': dry_run, 'quiet': quiet})
 
         for case in cases:
-            my_get_args = MyArgs(dry_run=case['dry_run'], quiet=case['quiet'])
+            my_get_args = MyArgs(dry_run=case['dry_run'], quiet=case['quiet'],
+                                 re_api_url=RE_API_URL, data_dir=None,
+                                 auth_token=AUTH_TOKEN)
             with patch('argparse.ArgumentParser.parse_args', return_value=my_get_args):
                 args = get_args()
                 self.assertEqual(args.dry_run, case['dry_run'])
@@ -218,7 +227,8 @@ class TestDataSourcesImporter(unittest.TestCase):
         using the default data files, succeeds.
         """
         from importers.data_sources import importer
-        my_get_args = MyArgs(dry_run=True, quiet=True)
+        my_get_args = MyArgs(dry_run=True, quiet=True, re_api_url=RE_API_URL,
+                             data_dir=None, auth_token=AUTH_TOKEN)
         with patch('argparse.ArgumentParser.parse_args', return_value=my_get_args):
             with patch.object(importer, '__name__', '__main__'):
                 with self.assertRaises(SystemExit) as se:
